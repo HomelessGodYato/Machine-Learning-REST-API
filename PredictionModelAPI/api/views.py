31a871fb32719a4695a8d971788259ce280c3978
HomelessGodYato <yaroslavbanchenko@gmail.com>
2022-08-14 12:27:44 +0200

    Added more views

--- PredictionModelAPI/api/views.py
+++ PredictionModelAPI/api/views.py
@@ -4,10 +4,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.views.decorators.csrf import csrf_exempt
-from .models import RequestedFeatures
-from .serializers import RequestedFeaturesSerializer
+from .models import RequestedFeatures, PredictedPrice
+from .serializers import RequestedFeaturesSerializer, PredictedPriceSerializer
 from django.http import JsonResponse
-
 
 
 class PricePredictionRandomForestRegressor(APIView):
@@ -36,12 +35,19 @@ class PricePredictionRandomForestRegressor(APIView):
         )
 
         input_data = np.array(input_data).reshape(1, -1)
-        # scaler = StandardScaler()
-        # scaler.fit(input_data)
-        # input_data = scaler.transform(input_data)
         model = ApiConfig.model
         price_predicted = model.predict(input_data)
         price_predicted = np.round(price_predicted, 1)
+        feature_serializer = RequestedFeaturesSerializer(features_save)
+        feature_id = feature_serializer.data.get('id')
+        print(feature_id)
+        requested_feature = RequestedFeatures.objects.get(id=feature_id)
+        print(requested_feature)
+        prediction_object = PredictedPrice.objects.create(
+            request_id=requested_feature,
+            predicted_price=price_predicted
+        )
+        print("saved")
         response_dict = {"Predicted Price": price_predicted}
         return Response(response_dict, status=200)
 
@@ -50,4 +56,22 @@ class GetAllRequestedFeatures(APIView):
     def get(self, request):
         features = RequestedFeatures.objects.all()
         serializer = RequestedFeaturesSerializer(features, many=True)
+        return JsonResponse(data=serializer.data, safe=False, status=status.HTTP_200_OK)
+
+
+class GetAllPredictions(APIView):
+    def get(self, request):
+        predictions = PredictedPrice.objects.all()
+        serializer = PredictedPriceSerializer(predictions, many=True)
+        return JsonResponse(data=serializer.data, safe=False, status=status.HTTP_200_OK)
+
+
+class GetOnePrediction(APIView):
+    def get(self, request, pk):
+        try:
+            prediction = PredictedPrice.objects.get(pk=pk)
+        except PredictedPrice.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = PredictedPriceSerializer(prediction)
         return JsonResponse(data=serializer.data, safe=False, status=status.HTTP_200_OK)
