47386402343ea7452f3ab8525c694d7b7fbd6d0b
HomelessGodYato <yaroslavbanchenko@gmail.com>
2022-08-14 16:13:49 +0200

    Changed views skeleton

--- PredictionModelAPI/api/views.py
+++ PredictionModelAPI/api/views.py
@@ -1,55 +1,15 @@
+import json
+
 import numpy as np
 from .apps import ApiConfig
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.views.decorators.csrf import csrf_exempt
-from .models import RequestedFeatures, PredictedPrice
-from .serializers import RequestedFeaturesSerializer, PredictedPriceSerializer
+from .models import RequestedFeatures, PredictedPrice, JSONFile
+from .serializers import RequestedFeaturesSerializer, PredictedPriceSerializer, FileSerializer
 from django.http import JsonResponse
-
-
-class PricePredictionRandomForestRegressor(APIView):
-    def post(self, request):
-        data = request.data
-        input_data = [data['carat'],
-                      data['cut'],
-                      data['color'],
-                      data['clarity'],
-                      data['depth'],
-                      data['table'],
-                      data['x'],
-                      data['y'],
-                      data['z']]
-
-        features_save = RequestedFeatures.objects.create(
-            carat=input_data[0],
-            cut=ApiConfig.cut_enc.inverse_transform(np.array([input_data[1]])),
-            color=ApiConfig.color_enc.inverse_transform(np.array([input_data[2]])),
-            clarity=ApiConfig.clarity_enc.inverse_transform(np.array([input_data[3]])),
-            depth=input_data[4],
-            table=input_data[5],
-            x=input_data[6],
-            y=input_data[7],
-            z=input_data[8]
-        )
-
-        input_data = np.array(input_data).reshape(1, -1)
-        model = ApiConfig.model
-        price_predicted = model.predict(input_data)
-        price_predicted = np.round(price_predicted, 1)
-        feature_serializer = RequestedFeaturesSerializer(features_save)
-        feature_id = feature_serializer.data.get('id')
-        print(feature_id)
-        requested_feature = RequestedFeatures.objects.get(id=feature_id)
-        print(requested_feature)
-        prediction_object = PredictedPrice.objects.create(
-            request_id=requested_feature,
-            predicted_price=price_predicted
-        )
-        print("saved")
-        response_dict = {"Predicted Price": price_predicted}
-        return Response(response_dict, status=200)
+from .utils import read_json
 
 
 class GetAllRequestedFeatures(APIView):
@@ -75,3 +35,57 @@ class GetOnePrediction(APIView):
 
         serializer = PredictedPriceSerializer(prediction)
         return JsonResponse(data=serializer.data, safe=False, status=status.HTTP_200_OK)
+
+
+class PredictFromFile(APIView):
+    def post(self, request):
+        serializer = FileSerializer(data=request.FILES)
+        if serializer.is_valid():
+            serializer.save()
+
+            fileId = serializer.data.get('id')
+            fileObject = JSONFile.objects.get(id=fileId)
+
+            data = read_json(fileObject.file)
+            predictions = []
+            model = ApiConfig.model
+
+            for i in range(len(data)):
+                data_from_json = {k: v for k, v in data[i].items()}
+
+                input_data = [data_from_json['carat'],
+                              data_from_json['cut'],
+                              data_from_json['color'],
+                              data_from_json['clarity'],
+                              data_from_json['depth'],
+                              data_from_json['table'],
+                              data_from_json['x'],
+                              data_from_json['y'],
+                              data_from_json['z']]
+
+                input_data = np.array(input_data).reshape(1, -1)
+                print(input_data)
+                price_predicted = model.predict(input_data)
+                price_predicted = np.round(price_predicted, 1)
+                predictions.append({"Predicted price": price_predicted})
+
+                features_object = RequestedFeatures.objects.create(
+                    file_id=fileObject,
+                    carat=input_data[0][0],
+                    cut=ApiConfig.cut_enc.inverse_transform([input_data[0][1].astype(int)]),
+                    color=ApiConfig.color_enc.inverse_transform([input_data[0][2].astype(int)]),
+                    clarity=ApiConfig.clarity_enc.inverse_transform([input_data[0][3].astype(int)]),
+                    depth=input_data[0][4],
+                    table=input_data[0][5],
+                    x=input_data[0][6],
+                    y=input_data[0][7],
+                    z=input_data[0][8]
+                )
+                PredictedPrice.objects.create(
+                    features_id=features_object,
+                    predicted_price=price_predicted
+                )
+
+                returnObject = FileSerializer(instance=fileObject)
+            return Response(data=returnObject.data, status=status.HTTP_200_OK)
+        return Response(data=serializer.data, status=status.HTTP_400_BAD_REQUEST)
