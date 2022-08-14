b790c91a1b8a928b95231e4e9de325cf6364213f
HomelessGodYato <yaroslavbanchenko@gmail.com>
2022-08-14 18:56:42 +0200

    HOT FIX

--- ml_api_model.py
+++ ml_api_model.py
@@ -1,14 +1,24 @@
 import pandas as pd
-import pickle
-from sklearn.preprocessing import MinMaxScaler, StandardScaler, LabelEncoder
+import joblib
+from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
-import os
 import bz2
 
-
 data = pd.read_csv('diamond_prices_encoded.csv')
+
+cut_encoder = LabelEncoder()
+cut_encoder.fit(data['cut'])
+data['cut'] = cut_encoder.transform(data['cut'])
+
+color_enc = LabelEncoder()
+color_enc.fit(data['color'])
+data['color'] = color_enc.transform(data['color'])
+
+clarity_enc = LabelEncoder()
+clarity_enc.fit(data['clarity'])
+data['clarity'] = clarity_enc.transform(data['clarity'])
+data.to_csv('diamond_prices_encoded.csv')
 
 y = data['price']
 X = data.drop(['price'], axis=1)
@@ -17,14 +27,20 @@ X = data.drop(['price'], axis=1)
 # X = scaler.transform(X)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 model = RandomForestRegressor()
-
 model.fit(X_train.values, y_train.values)
-
 # print(model.score(X_test,y_test))
-# pickle.dump(cut_encoder, open('cut_encoder.pkl', 'wb'))
-# pickle.dump(color_enc, open('color_encoder.pkl', 'wb'))
-# pickle.dump(clarity_enc, open('clarity_encoder.pkl', 'wb'))
-pickle.dump(model, open('random_forest_regressor.pkl', 'wb'))
-compressed_model = bz2.BZ2File("random_forest_regressor_compressed.pkl", 'wb')
-pickle.dump(model, compressed_model)
-# print(os.path.getsize("random_forest_regressor_compressed.pkl"))
+joblib.dump(cut_encoder, "./PredictionModelAPI/ml/Encoders/cut_encoder.pkl", compress=3)
+joblib.dump(color_enc, "./PredictionModelAPI/ml/Encoders/color_encoder.pkl", compress=3)
+joblib.dump(clarity_enc, "./PredictionModelAPI/ml/Encoders/clarity_encoder.pkl", compress=3)
+joblib.dump(model, "./PredictionModelAPI/ml/Model/random_forest_regressor.pkl", compress=3)
+# with bz2.BZ2File('cut_encoder.pbz2', 'wb') as cut_enc:
+#     pickle.dump(cut_encoder, cut_enc)
+#
+# with bz2.BZ2File('color_encoder.pbz2', 'wb') as color_encoder:
+#     pickle.dump(color_enc, color_encoder)
+#
+# with bz2.BZ2File('clarity_encoder.pbz2', 'wb') as clarity_encoder:
+#     pickle.dump(clarity_enc, clarity_encoder)
+#
+# with bz2.BZ2File('random_forest_regressor.pbz2', 'wb') as rf_cls:
+#     pickle.dump(model, rf_cls)
