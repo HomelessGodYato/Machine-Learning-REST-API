54c5fd5d165628bb01940766fff8b0d80726e4d2
HomelessGodYato <yaroslavbanchenko@gmail.com>
2022-08-14 12:27:14 +0200

    Added additional endpoints

--- PredictionModelAPI/api/urls.py
+++ PredictionModelAPI/api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import PricePredictionRandomForestRegressor
+from .views import PricePredictionRandomForestRegressor, GetAllRequestedFeatures, GetAllPredictions,GetOnePrediction
 
 urlpatterns = [
-    path('price/', PricePredictionRandomForestRegressor.as_view(), name = 'price_prediction'),
+    path('price/', PricePredictionRandomForestRegressor.as_view(), name='price_prediction'),
+    path('features/', GetAllRequestedFeatures.as_view(), name='all_requested_features'),
+    path('predictions/', GetAllPredictions.as_view(), name='all_predictions'),
+    path('prediction/<int:pk>/',GetOnePrediction.as_view(),name = 'one_prediction')
 ]
