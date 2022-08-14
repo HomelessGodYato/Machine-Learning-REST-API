851e81087ef6fdb6b237f119fd3174480d4aad29
HomelessGodYato <yaroslavbanchenko@gmail.com>
2022-08-14 16:12:18 +0200

    Additional serializer

--- PredictionModelAPI/api/serializers.py
+++ PredictionModelAPI/api/serializers.py
@@ -1,12 +1,5 @@
 from rest_framework import serializers
-from .models import RequestedFeatures, PredictedPrice
-
-
-class RequestedFeaturesSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = RequestedFeatures
-
-        fields = "__all__"
+from .models import RequestedFeatures, PredictedPrice, JSONFile
 
 
 class PredictedPriceSerializer(serializers.ModelSerializer):
@@ -14,3 +7,33 @@ class PredictedPriceSerializer(serializers.ModelSerializer):
         model = PredictedPrice
 
         fields = "__all__"
+
+
+class RequestedFeaturesSerializer(serializers.ModelSerializer):
+    prediction = PredictedPriceSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = RequestedFeatures
+
+        fields = ["id",
+                  "carat",
+                  "cut",
+                  "color",
+                  "clarity",
+                  "depth",
+                  "table",
+                  "x",
+                  "y",
+                  "z",
+                  "prediction"]
+
+
+class FileSerializer(serializers.ModelSerializer):
+    features = RequestedFeaturesSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = JSONFile
+        fields = ["id",
+                  "file",
+                  "created",
+                  "features"]
