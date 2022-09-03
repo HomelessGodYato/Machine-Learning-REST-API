f86ff2d572dd4b274abe435f676abd5154432483
HomelessGodYato <yaroslavbanchenko@gmail.com>
2022-09-03 18:52:23 +0200

    Configured accuracy.

--- PredictionModelAPI/api/apps.py
+++ PredictionModelAPI/api/apps.py
@@ -7,12 +7,18 @@ from django.conf import settings
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
-    MODEL_FILE = os.path.join(settings.MODELS, "random_forest_regressor.pkl")
+    RF_MODEL_FILE = os.path.join(settings.MODELS, "random_forest_regressor.pkl")
+
+    RF_SCORE_FILE = os.path.join(settings.SCORES, "rfc_score.pkl")
+    
     COLOR_ENCODER_FILE = os.path.join(settings.ENCODERS, "color_encoder.pkl")
     CUT_ENCODER_FILE = os.path.join(settings.ENCODERS, "cut_encoder.pkl")
     CLARITY_ENCODER_FILE = os.path.join(settings.ENCODERS, "clarity_encoder.pkl")
 
-    model = joblib.load(MODEL_FILE)
+    rf_model = joblib.load(RF_MODEL_FILE)
+
+    rf_score = joblib.load(RF_SCORE_FILE)
+
     color_enc = joblib.load(COLOR_ENCODER_FILE)
     cut_enc = joblib.load(CUT_ENCODER_FILE)
     clarity_enc = joblib.load(CLARITY_ENCODER_FILE)
