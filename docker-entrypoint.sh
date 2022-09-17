c7ccd51e18454dfd976e5021d4e6188e184eb046
HomelessGodYato <yaroslavbanchenko@gmail.com>
2022-09-17 17:04:48 +0200

    Entrypoint for auto migrations on starting container.

---
+++ docker-entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+echo "Making migrations of the django app"
+
+while ! python manage.py makemigrations 2>&1; do
+  echo "Making migrations of the django app"
+  sleep 3
+done
+
+echo "Migrate the Database at startup of project"
+
+# Wait for few minute and run db migraiton
+while ! python manage.py migrate --run-syncdb  2>&1; do
+   echo "Migration is in progress status"
+   sleep 3
+done
+
+echo "Django docker is fully configured successfully."
+
+exec "$@"
